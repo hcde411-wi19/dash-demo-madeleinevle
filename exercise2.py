a700ba8269bea35a212cb9f0763fadfbfa377e87
Madeleine Le <madeleinele@D-10-19-33-30.dhcp4.washington.edu>
2019-02-14 14:03:04 -0800

    adding code from exercise 1

--- exercise2.py
+++ exercise2.py
@@ -7,3 +7,7 @@ import dash_html_components as html
 # Exercise 2
 # Create another visualization of your choice of data and chart type.
 # You can use pandas to help loading data, or just hard-coded the data is fine.
+
+# Data used
+ped_n = [26884, 26444, 25876, 24368, 23403, 36894, 32792]
+bike_n = [52401, 49449, 50209, 49692, 47971, 69437, 67752]
