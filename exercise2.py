ce7f96bd3f8b59d63b93dc582a8a2f9e9e287978
Madeleine Le <madeleinele@D-10-18-254-72.dhcp4.washington.edu>
2019-02-19 13:13:10 -0800

    adding code for exercise2

--- exercise2.py
+++ exercise2.py
@@ -2,6 +2,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # Exercise 2
@@ -9,8 +11,51 @@ import dash_html_components as html
 # You can use pandas to help loading data, or just hard-coded the data is fine.
 
 # Data used
-ped_n = [26884, 26444, 25876, 24368, 23403, 36894, 32792]
-bike_n = [52401, 49449, 50209, 49692, 47971, 69437, 67752]
-
 app = dash.Dash(__name__, static_folder='static')
+file = pd.read_csv('static/cereal.csv')
+
+app.layout = html.Div(children=[
+
+    # set the page heading
+    html.H1(children='Sugar in Each Serving of Cereal'),
+
+    # set the description underneath the heading
+    html.Div(children='''
+        A demo to show the amount of sugar in a serving of cereal
+    '''),
+
+    # append the visualization to the page
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            # configure the data
+            'data': [
+                # This is how we define a scatter plot. Note that it also uses "go.Scatter",
+                # but with the mode to be only "markers"
+                go.Scatter(
+                    x=file['cups'],
+                    y=file['sugars'],
+                    mode='markers',
+                    text=file['name'],  # This line sets the cereal name as the points' labels.
+                    marker={
+                        'size': 10,
+                        'opacity': 0.7  # By making the points a bit transparent, it can alleviate the occlusion issue
+                    }
+                )
+            ],
+            'layout': {
+                'title': 'Cereal Correlations',
+                # It is always a good practice to have axis labels.
+                # This is especially important in this case as the numbers are not trivial
+                'xaxis': {'title': 'Cups'},
+                'yaxis': {'title': 'Sugars'},
+            }
+        }
+    )
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
 
--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # Note: import the app of what you are working on
 #from initial_demo import app
-from exercise1 import app
-# from exercise2 import app
+#from exercise1 import app
+from exercise2 import app
 # from exercise3 import app
 #from vis_scatter_plot import app
 
